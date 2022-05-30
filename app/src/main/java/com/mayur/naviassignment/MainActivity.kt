3173cb4db69dcb957f471d6b852fec0ce67c8000
mayurgetmega <56244338+mayurgetmega@users.noreply.github.com>
2022-05-30 23:08:16 +0530

    minor changes

--- app/src/main/java/com/mayur/naviassignment/MainActivity.kt
+++ app/src/main/java/com/mayur/naviassignment/MainActivity.kt
@@ -2,23 +2,37 @@ package com.mayur.naviassignment
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.activity.viewModels
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.tooling.preview.Preview
 import com.mayur.naviassignment.components.setContentWithAppTheme
+import com.mayur.naviassignment.data.RepoProvider
+import com.mayur.naviassignment.pulls.PullsViewModel
 import com.mayur.naviassignment.ui.theme.NaviAssignmentTheme
 
+// Pass auth token and make api call
+// Create Auth interceptor
+//
+
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<PullsViewModel> {
+        PullsViewModel.Factory(RepoProvider.pullsRepository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentWithAppTheme {
-            Greeting("Android")
+            ClosePullsUI(viewModel = viewModel)
         }
+    }
+}
+
+@Composable
+fun ClosePullsUI(viewModel: PullsViewModel) {
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getPulls()
     }
 }
 
--- app/src/main/java/com/mayur/naviassignment/data/RepoProvider.kt
+++ app/src/main/java/com/mayur/naviassignment/data/RepoProvider.kt
@@ -1,6 +1,8 @@
 package com.mayur.naviassignment.data
 
+import com.mayur.naviassignment.data.pulls.PullsRepository
+
 
 object RepoProvider {
-
+    val pullsRepository by lazy { PullsRepository(ApiServiceProvider.pullsService) }
 }
