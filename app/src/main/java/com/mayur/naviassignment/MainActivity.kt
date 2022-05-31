77b775ffa25c5d08342cc7a41a747489c2f7338e
mayurgetmega <56244338+mayurgetmega@users.noreply.github.com>
2022-05-31 15:54:50 +0530

    showing PR details + formatted date

--- app/src/main/java/com/mayur/naviassignment/MainActivity.kt
+++ app/src/main/java/com/mayur/naviassignment/MainActivity.kt
@@ -3,20 +3,36 @@ package com.mayur.naviassignment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mayur.naviassignment.components.setContentWithAppTheme
 import com.mayur.naviassignment.data.RepoProvider
+import com.mayur.naviassignment.data.pulls.PullRequest
 import com.mayur.naviassignment.pulls.PullsViewModel
 import com.mayur.naviassignment.ui.theme.NaviAssignmentTheme
+import java.text.SimpleDateFormat
 
 // Pass auth token and make api call
 // Create Auth interceptor
-//
+// Circle Avatar
+// beautify card
+// Date format
 
 class MainActivity : ComponentActivity() {
+    val sdf = SimpleDateFormat("d MMMM yy")
+
     private val viewModel by viewModels<PullsViewModel> {
         PullsViewModel.Factory(RepoProvider.pullsRepository)
     }
@@ -24,15 +40,50 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentWithAppTheme {
-            ClosePullsUI(viewModel = viewModel)
+            ClosePullsUI(
+                viewModel = viewModel,
+                sdf = sdf
+            )
+        }
+
+    }
+}
+
+@Composable
+fun ClosePullsUI(viewModel: PullsViewModel, sdf: SimpleDateFormat) {
+    val pulls by viewModel.pulls
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getPulls()
+    }
+
+    pulls?.let {
+        ClosePullsUI(it, sdf)
+    }
+}
+
+@Composable
+fun ClosePullsUI(pulls: List<PullRequest>, sdf: SimpleDateFormat) {
+    LazyColumn {
+        items(pulls) { pull ->
+            ClosePullUI(pull, sdf)
         }
     }
 }
 
 @Composable
-fun ClosePullsUI(viewModel: PullsViewModel) {
-    LaunchedEffect(key1 = Unit) {
-        viewModel.getPulls()
+fun ClosePullUI(pull: PullRequest, sdf: SimpleDateFormat) {
+    Card {
+        Column {
+            Text(text = pull.title)
+            Text(text = sdf.format(pull.createdAt))
+            Text(text = sdf.format(pull.closedAt ?: ""))
+            Text(text = pull.user.username)
+            // Glide image
+            Divider(modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp))
+        }
     }
 }
 
--- app/src/main/java/com/mayur/naviassignment/pulls/PullsViewModel.kt
+++ app/src/main/java/com/mayur/naviassignment/pulls/PullsViewModel.kt
@@ -1,12 +1,15 @@
 package com.mayur.naviassignment.pulls
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.mayur.naviassignment.data.pulls.PullRequest
 import com.mayur.naviassignment.data.pulls.PullsRepository
 import kotlinx.coroutines.launch
 
 class PullsViewModel(private val repository: PullsRepository): ViewModel() {
+    var pulls = mutableStateOf<List<PullRequest>?>(listOf())
 
     fun getPulls() {
         viewModelScope.launch {
@@ -17,7 +20,14 @@ class PullsViewModel(private val repository: PullsRepository): ViewModel() {
                             "result?.createdAt ${result?.getOrNull(0)?.createdAt}" +
                                     " result?.user ${result?.getOrNull(0)?.user}"
                         )
+
+                        pulls.value = result
                     }
+                    isError() ->  {
+
+                    }
+
+                    inProgress() -> {}
                 }
             }
         }
